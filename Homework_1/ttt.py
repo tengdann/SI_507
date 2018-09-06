a94a839492136cf40045f8a860efbbd9f112e3bb
tengdann <tengdann@umich.edu>
2018-09-06 12:01:57 -0400

    Works, but needs refinement

--- Homework_1/ttt.py
+++ Homework_1/ttt.py
@@ -4,8 +4,7 @@ SI 507 Fall 2018 Homework 1
 
 # Create board - Setup the data structure for storing board data
     
-board_data = [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ] # Indexed from bottom left; refer to keypad on keyboard 
-game_status = True
+bd = [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ] # Indexed from bottom left; refer to keypad on keyboard 
     
 print ("Welcome to tic-tac-toe! Moves will correspond to the numeric keypad on your keyboard. Enjoy!")
 
@@ -15,97 +14,98 @@ print ("Welcome to tic-tac-toe! Moves will correspond to the numeric keypad on y
 '''
 This function will take in current board data and print out the board in the console as shown 
 in the instruction.
-parameter: board_data - a data structure used for holding current moves on the board
+parameter: bd - a data structure used for holding current moves on the board
 return: None
 '''
-def print_board(board_data):
-	print ("CURRENT BOARD: ")
-	print (' ' + board_data[6] + ' | ' + board_data[7] + ' | ' + board_data[8] + ' ')
-	print ('-----------')
-	print (' ' + board_data[3] + ' | ' + board_data[4] + ' | ' + board_data[5] + ' ')
-	print ('-----------')
-	print (' ' + board_data[0] + ' | ' + board_data[1] + ' | ' + board_data[2] + ' ')
+def print_board(bd):
+    print ("CURRENT BOARD: ")
+    print (' ' + bd[6] + ' | ' + bd[7] + ' | ' + bd[8] + ' ')
+    print ('-----------')
+    print (' ' + bd[3] + ' | ' + bd[4] + ' | ' + bd[5] + ' ')
+    print ('-----------')
+    print (' ' + bd[0] + ' | ' + bd[1] + ' | ' + bd[2] + ' ')
 
-# Step 2: Player 1 moves; check to see if valid; edits board_data
-def player_one_move(board_data):
+# Step 2: Player 1 moves; check to see if valid; edits bd
+def player_one_move(bd):
     player_move = int(input("Player 1's move > "))
 
-    while board_data[player_move - 1] != ' ':
+    while bd[player_move - 1] != ' ':
         print ("Sorry, invalid move!")
         player_move = int(input("Player 1's move > "))
         
-    board_data[player_move - 1] = 'X'
+    bd[player_move - 1] = 'X'
 
 # Step 3: Print new board, and check if game is over
 
 # Step 4: Player 2 moves; check to see if valid
-def player_two_move(board_data):    
+def player_two_move(bd):    
     player_move = int(input("Player 2's move > "))
     
-    while board_data[player_move - 1] != ' ':
+    while bd[player_move - 1] != ' ':
         print ("Sorry, invalid move!")
         player_move = int(input("Player 2's move > "))
 
-    board_data[player_move - 1] = 'O'
+    bd[player_move - 1] = 'O'
 # Step 5: Print new board, and check if game is over
 
 # Step 6: Determine if game is over
 '''
 Take in the current board data and determine if one player wins the game or the game draws. If the game is over,
 terminate the loop, or continue the loop.
-parameter: board_data - current board data
+parameter: bd - current board data
 return: information about current game status
 '''
 
-def determine_over(board_data):
+def determine_over(bd):
     # Need to do 9 checks; more elegent method?
     
     # First row matching
-    if board_data[0] == board_data[1] == board_data[2]:
-		game_over_text(board_data[1])
-		return False
+    if (' ' not in (bd[0], bd[1], bd[2]) and bd[0] == bd[1] == bd[2]):
+        game_over_text(bd[1])
+        return False
         
     # Second row matching
-    if board_data[3] == board_data[4] == board_data[5]:
-		game_over_text(board_data[4])
-		return False
+    elif (' ' not in (bd[3], bd[4], bd[5]) and bd[3] == bd[4] == bd[5]):
+        game_over_text(bd[4])
+        return False
         
     # Third row matching
-    if board_data[6] == board_data[7] == board_data[8]:
-		game_over_text(board_data[7])
-		return False
+    elif (' ' not in (bd[6], bd[7], bd[8]) and bd[6] == bd[7] == bd[8]):
+        game_over_text(bd[7])
+        return False
         
     # First column matching
-    if board_data[0] == board_data[3] == board_data[6]:
-		game_over_text(board_data[3])
-		return False
+    elif (' ' not in (bd[0], bd[3], bd[6]) and bd[0] == bd[3] == bd[6]):
+        game_over_text(bd[3])
+        return False
         
     # Second column matching
-    if board_data[1] == board_data[4] == board_data[7]:
-		game_over_text(board_data[4])
-		return False
+    elif (' ' not in (bd[1], bd[4], bd[7]) and bd[1] == bd[4] == bd[7]):
+        game_over_text(bd[4])
+        return False
         
     # Third column matching
-    if board_data[2] == board_data[5] == board_data[8]:
-		game_over_text(board_data[5])
-		return False
+    elif (' ' not in (bd[2], bd[5], bd[8]) and bd[2] == bd[5] == bd[8]):
+        game_over_text(bd[5])
+        return False
         
     # TL-BR diagonal matching
-    if board_data[6] == board_data[4] == board_data[2]:
-		game_over_text(board_data[4])
-		return False
+    elif (' ' not in (bd[6], bd[4], bd[2]) and bd[6] == bd[4] == bd[2]):
+        game_over_text(bd[4])
+        return False
         
     # TR-BL diagonal matching
-    if board_data[8] == board_data[4] == board_data[0]:
-		game_over_text(board_data[4])
-		return False
+    elif (' ' not in (bd[8], bd[4], bd[0]) and bd[8] == bd[4] == bd[0]):
+        game_over_text(bd[4])
+        return False
         
     # Cat's game
-    if board_data.count(' ') == 1:
+    elif bd.count(' ') == 1:
         print("Cat's game! No one wins :(")
         return False
-		
-	return True
+
+    else:
+        return True
     
     
 def game_over_text(str):
@@ -119,25 +119,24 @@ def game_over_text(str):
 
 while True:
     print()
-    print_board(board_data)
+    print_board(bd)
 
     # Step 2: Player 1 moves; check to see if valid
         
-    player_one_move(board_data)
+    player_one_move(bd)
 
     # Step 3: Print new board and check if game is over
     
-    print_board(board_data)
-    if not determine_over(board_data): break
+    print_board(bd)
+    if not determine_over(bd): break
 
     # Step 4: Player 2 moves; check to see if valid
     
-    player_two_move(board_data)
+    player_two_move(bd)
     
     # Step 5: Print new board and check if game is over
     
-    print_board(board_data)
-    if not determine_over(board_data): break
+    print_board(bd)
     
     # Step 6: Determine if the game is over
-    if not determine_over(board_data): break
+    if not determine_over(bd): break
