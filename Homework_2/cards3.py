d185a1c7dd931251c03512cd8b969a2fcd2468ce
tengdann <tengdann@umich.edu>
2018-09-13 20:26:49 -0400

    Finished EC2

--- Homework_2/cards3.py
+++ Homework_2/cards3.py
@@ -1,4 +1,5 @@
 import unittest
+from operator import itemgetter
 
 # Because what even is importing from another file
 class Card(object):
@@ -61,7 +62,26 @@ class Deck(object):
     # Deal out specified number of cards to number of hands
     # If per_hand is -1, deal all cards in deck, regardless if unequal hands
     def deal(self, num_hands, per_hand):
-        pass
+        list_hands = []
+        
+        for x in range(0, num_hands):
+            list_hands.append(Hand([]))
+        
+        if per_hand != -1:
+            for x in range(0, per_hand):
+                for hand in list_hands:
+                    hand.draw(self)
+            
+            return list_hands
+        else:
+            while len(self.cards) != 0:
+                for hand in list_hands:
+                    try:
+                        hand.draw(self)
+                    except:
+                        pass
+            
+            return list_hands
 
 
 class Hand(object):
@@ -97,9 +117,60 @@ class Hand(object):
     
     # Looks through hand, removing pairs of cards (rank, as per poker definition of pair)
     def remove_pairs(self):
-        pass        
+        hand_ranks = []
         
-class TestHand2(unittest.TestCase):
+        for card in self.hand:
+            hand_ranks.append((card.rank, self.hand.index(card)))
+        
+        # Itemgetter use taken from https://stackoverflow.com/questions/3121979/how-to-sort-list-tuple-of-lists-tuples
+        # Need to sort hand_ranks by first index of the (rank, index) tuple
+        hand_ranks.sort(key = itemgetter(0))
+        
+        self.hand.pop(hand_ranks[0][1])
+        self.hand.pop(hand_ranks[0][1])
+        
+        
+        
+        
+class TestEC2(unittest.TestCase):
+    def test_deal(self):
+        deck = Deck()
+        
+        # Test simple deal
+        test_list = deck.deal(num_hands = 3, per_hand = 5)
+        self.assertEqual(len(test_list), 3)
+        for hands in test_list:
+            self.assertEqual(len(hands.hand), 5)
+            
+        deck2 = Deck()
+            
+        # Test per_hand = -1
+        test_list2 = deck2.deal(num_hands = 5, per_hand = -1)
+        self.assertEqual(len(test_list2), 5)
+        for hands2 in test_list2:
+            # Should have at least 10 cards per deck
+            self.assertTrue(len(hands2.hand) >= 10)
+            
+        # Testing to see if first hand2 and last hand2 size are different
+        self.assertTrue(len(test_list2[0].hand) > len(test_list2[4].hand))
+        
+    def test_remove_pairs(self):
+        hand = Hand([Card(rank = 2), Card(suit = 1, rank = 2), Card(rank = 3), Card(suit = 1, rank = 3)])
+        hand.remove_pairs()
+        
+        # Test pair of rank in hand
+        self.assertEqual(len(hand.hand), 2)
+        self.assertEqual(str(hand.hand[0]), str(Card(rank = 3)))
+        self.assertEqual(str(hand.hand[1]), str(Card(suit = 1, rank = 3)))
+        
+        # Test three of a rank in hand
+        hand2 = Hand([Card(rank = 2), Card(suit = 1, rank = 2), Card(suit = 2, rank = 2), Card(rank = 3), Card(suit = 1, rank = 3)])
+        hand2.remove_pairs()
+        self.assertEqual(len(hand2.hand), 3)
+        self.assertEqual(str(hand2.hand[0]), str(Card(suit = 2, rank = 2)))
+        self.assertEqual(str(hand2.hand[1]), str(Card(rank = 3)))
+        self.assertEqual(str(hand2.hand[2]), str(Card(suit = 1, rank = 3)))
+        
 
         
 if __name__ == "__main__":
