a76fea0146bac1c788c3042c64f8bbc5be3e11f9
Danny Teng <tengdann@umich.edu>
2018-09-25 11:41:52 -0400

    Media testing complete

--- Project_1/proj1_f18.py
+++ Project_1/proj1_f18.py
@@ -1,10 +1,40 @@
 class Media:
-
-    def __init__(self, title="No Title", author="No Author"):
+    def __init__(self, title="No Title", author="No Author", year = "No Year"):
         self.title = title
         self.author = author
+        self.year = str(year)
+        
+    def __str__(self):
+        return self.title + ' by ' + self.author + ' (%s)' % self.year
+        
+    def __len__(self):
+        return 0
+        
+class Song(Media):
+    def __init__(self, title, author, year, album = "No album", genre = "No genre", track_length = 0):
+        super().__init__(title, author, year)
+        self.album = album
+        self.genre = genre
+        self.track_length = track_length
+        
+    def __str__(self):
+        super().__str__() + ' [%s]' % self.genre
+        
+    def __len__(self):
+        return self.track_length
+        
+class Movie(Media):
+    def __init__(self, title, author, year, rating = "No rating", movie_length = 0):
+        super().__init__(title, author, year)
+        self.rating = rating
+        self.movie_length = movie_length
+        
+    def __str__(self):
+        super().__str__() + ' [%s]' % self.rating
+        
+    def __len__(self):
+        return self.movie_length
 
-## Other classes, functions, etc. should go here
 
 if __name__ == "__main__":
     # your control code for Part 4 (interactive search) should go here
--- Project_1/proj1_f18_test.py
+++ Project_1/proj1_f18_test.py
@@ -6,11 +6,37 @@ class TestMedia(unittest.TestCase):
     def testConstructor(self):
         m1 = proj1.Media()
         m2 = proj1.Media("1999", "Prince")
+        m3 = proj1.Media("Kamikaze", "Eminem", 2018)
 
         self.assertEqual(m1.title, "No Title")
         self.assertEqual(m1.author, "No Author")
+        self.assertEqual(m1.year, "No Year")
+        
         self.assertEqual(m2.title, "1999")
         self.assertEqual(m2.author, "Prince")
+        self.assertEqual(m2.year, "No Year")
+        
+        self.assertEqual(m3.title, "Kamikaze")
+        self.assertEqual(m3.author, "Eminem")
+        self.assertEqual(m3.year, "2018")
+        
+    def teststr(self):
+        m1 = proj1.Media()
+        m2 = proj1.Media("1999", "Prince")
+        m3 = proj1.Media("Kamikaze", "Eminem", 2018)
+        
+        self.assertEqual(str(m1), "No Title by No Author (No Year)")
+        self.assertEqual(str(m2), "1999 by Prince (No Year)")
+        self.assertEqual(str(m3), "Kamikaze by Eminem (2018)")
+        
+    def testlen(self):
+        m1 = proj1.Media()
+        m2 = proj1.Media("1999", "Prince")
+        m3 = proj1.Media("Kamikaze", "Eminem", 2018)
+        
+        self.assertEqual(len(m1), 0)
+        self.assertEqual(len(m2), 0)
+        self.assertEqual(len(m3), 0)
 
 
 unittest.main()
