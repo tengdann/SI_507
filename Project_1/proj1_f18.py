fc3853407b0a5b2cfdc2fcc309c10d480549f4a5
Danny Teng <tengdann@umich.edu>
2018-09-25 10:57:26 -0400

    Initial Project 1 commit

---
+++ Project_1/proj1_f18.py
@@ -0,0 +1,11 @@
+class Media:
+
+    def __init__(self, title="No Title", author="No Author"):
+        self.title = title
+        self.author = author
+
+## Other classes, functions, etc. should go here
+
+if __name__ == "__main__":
+    # your control code for Part 4 (interactive search) should go here
+    pass
---
+++ Project_1/proj1_f18_test.py
@@ -0,0 +1,16 @@
+import unittest
+import proj1_f18 as proj1
+
+class TestMedia(unittest.TestCase):
+
+    def testConstructor(self):
+        m1 = proj1.Media()
+        m2 = proj1.Media("1999", "Prince")
+
+        self.assertEqual(m1.title, "No Title")
+        self.assertEqual(m1.author, "No Author")
+        self.assertEqual(m2.title, "1999")
+        self.assertEqual(m2.author, "Prince")
+
+
+unittest.main()
