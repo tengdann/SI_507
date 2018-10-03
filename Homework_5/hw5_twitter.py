16f888ea724908b996163056a7261d674c2bf5b5
Danny Teng <tengdann@umich.edu>
2018-10-03 10:14:41 -0400

    Initial push of HW5

---
+++ Homework_5/hw5_twitter.py
@@ -0,0 +1,43 @@
+from requests_oauthlib import OAuth1
+import json
+import sys
+import requests
+import secret_data # file that contains OAuth credentials
+# Uncomment following two lines after you install nltk
+# import nltk 
+# from nltk.corpus import stopwords
+
+## SI 507 - HW5
+## COMMENT WITH:
+## Your section day/time:
+## Any names of people you worked with on this assignment:
+
+#usage should be python3 hw5_twitter.py <username> <num_tweets>
+username = sys.argv[1]
+num_tweets = sys.argv[2]
+
+consumer_key = secret_data.CONSUMER_KEY
+consumer_secret = secret_data.CONSUMER_SECRET
+access_token = secret_data.ACCESS_KEY
+access_secret = secret_data.ACCESS_SECRET
+
+#Code for OAuth starts
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
+requests.get(url, auth=auth)
+#Code for OAuth ends
+
+#Write your code below:
+#Code for Part 1:Get Tweets
+
+#Code for Part 2:Analyze Tweets
+
+#Code for Part 3:Caching
+
+if __name__ == "__main__":
+    if not consumer_key or not consumer_secret:
+        print("You need to fill in client_key and client_secret in the secret_data.py file.")
+        exit()
+    if not access_token or not access_secret:
+        print("You need to fill in this API's specific OAuth URLs in this file.")
+        exit()
