b6659444abd2f86e5ec09aaca0d1aa9bc8b2ef7f
Danny Teng <tengdann@umich.edu>
2018-10-24 10:07:52 -0400

    Initial HW7 commit

---
+++ Homework_7/hw7_part1.py
@@ -0,0 +1,14 @@
+# 507 Homework 7 Part 1
+import requests
+import json
+from bs4 import BeautifulSoup
+
+#### Your Part 1 solution goes here ####
+def get_umsi_data(page):
+    #### Implement your function here ####
+    pass
+
+#### Execute funciton, get_umsi_data, here ####
+
+
+#### Write out file here #####
---
+++ Homework_7/hw7_part2.py
@@ -0,0 +1,10 @@
+
+# 507 Homework 7 Part 2
+
+count = 0
+#### Your Part 2 solution goes here ####
+
+
+
+#### Your answer output (change the value in the variable, count)####
+print('The number of PhD students: ', count)
