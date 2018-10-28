796b132e28692ad217ca22117a2329124819eda7
tengdann <tengdann@umich.edu>
2018-10-28 17:37:14 -0400

    Fixed ec1

--- Homework_7/hw7_ec1.py
+++ Homework_7/hw7_ec1.py
@@ -16,22 +16,38 @@ with requests_cache.disabled():
 
 freep_soup = BeautifulSoup(freep_text, 'html.parser')
 
+headline = []
 headlines_links = []
 
 raw_links = freep_soup.find_all('a', itemprop = 'url', limit = 10)
 for raw_link in raw_links:
+    try:
+        headline.append(raw_link.find('span', class_ = 'hgpm-list-hed js-asset-headline placeholder-bg ').text)
+    except:
+        headlines.append(None)
     headlines_links.append(baseurl + raw_link['href'])
 
 information = {}
+author = []
+published = []
     
 for i in headlines_links:
     headline_text = requests.get(i).text
     headline_soup = BeautifulSoup(headline_text, 'html.parser')
     
-    headline = headline_soup.find('h1', class_ = 'asset-headline speakable-headline').text
-    author = headline_soup.find('span', class_ = 'asset-metabar-author asset-metabar-item').text.split(',')[0]
-    published = headline_soup.find('span', class_ = 'asset-metabar-time asset-metabar-item nobyline').text.split('|')[0].replace('\n', '')[22:] # I know its a mess, ignore the chaining of functions; I'm lazy as fuuuucccck
-    information[i] = {'headline': headline, 'author': author, 'published': published}
+    try:
+        author.append(headline_soup.find('span', class_ = 'asset-metabar-author asset-metabar-item').text.split(',')[0])
+    except:
+        author.append(None)
+        
+    try:
+        published.append(headline_soup.find('span', class_ = 'asset-metabar-time asset-metabar-item nobyline').text.split('|')[0].replace('\n', '')[22:]) # I know its a mess, ignore the chaining of functions; I'm lazy as fuuuucccck
+    except:
+        published.append(None)
+
+for i,h,a,p in zip(headlines_links,headline, author, published):
+    if i is not None and h is not None and a is not None and p is not None:
+        information[i] = {'headline': h, 'author': a, 'published': p}
 
 with open('freep.csv', mode = 'w') as csv_file:
     fieldnames = ['headline', 'author', 'published']
@@ -40,5 +56,5 @@ with open('freep.csv', mode = 'w') as csv_file:
     writer.writeheader()
     for i in information:
         writer.writerow(information[i])
-    
+        
 print('The script took {0} seconds to run.'.format(time.time() - startTime))    
